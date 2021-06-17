be5d60d1f73491699e3a026f3edcae021fe078ae
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-17 11:12:24 +0530

    remove imports

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 import Dashboard from "./views/Dashboard.vue";
 import Tables from "./views/Tables.vue";
-import UIElements from "./views/UIElements.vue";
 import Login from "./views/Login.vue";
-import Modal from "./views/Modal.vue";
-import Blank from "./views/Blank.vue";
-import NotFound from "./views/NotFound.vue";
 
 const routes: RouteRecordRaw[] = [
   {
