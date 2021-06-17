e40d9eac81e80b45e5075e5727177d337910b89c
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-17 11:55:19 +0530

    remove header imports

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -16,11 +16,9 @@
 import { defineComponent, ref } from "vue";
 
 import Sidebar from "./Sidebar.vue";
-import Header from "./Header.vue";
 
 export default defineComponent({
   components: {
-    Header,
     Sidebar,
   },
 });
