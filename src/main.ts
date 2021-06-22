ee5e9d5129110e13aa94244138b5bd9b03b2d046
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-22 17:29:08 +0530

    import index css

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/main.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
+import "./index.css";
 
-import DashboardLayout from './components/DashboardLayout.vue';
-import EmptyLayout from './components/EmptyLayout.vue';
+import DashboardLayout from "./components/DashboardLayout.vue";
+import EmptyLayout from "./components/EmptyLayout.vue";
 
 const app = createApp(App);
 
-app.component('default-layout', DashboardLayout);
-app.component('empty-layout', EmptyLayout);
+app.component("default-layout", DashboardLayout);
+app.component("empty-layout", EmptyLayout);
 
 app.use(router);
-app.mount('#app');
+app.mount("#app");
